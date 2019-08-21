235ab3a04a0ae684d9b39664f88cf47adb30bbe6
苏震巍 <zsu@senparc.com>
2019-08-21 10:58:27 +0800

    添加基础功能代码（已经可以使用）

---
+++ Leader/SignalrPlaySongSample/SignalrPlaySongSample.LeaderClient/SongHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrPlaySongSample.LeaderClient
+{
+    public class SongHub : Hub
+    {
+        public async Task RequestPlay()
+        {
+            await Clients.All.SendAsync("Play");
+        }
+
+        public async Task RequestPause()
+        {
+            await Clients.All.SendAsync("Pause");
+        }
+    }
+}
---
+++ Leader/SignalrPlaySongSample/SignalrPlaySongSample.LeaderClient/wwwroot/js/site.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+var connection;
+
+
+document.getElementById("play").addEventListener("click", function (event) {
+    connection.invoke("RequestPlay").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("pause").addEventListener("click", function (event) {
+    connection.invoke("RequestPause").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("startConnection").addEventListener("click", function (event) {
+     connection = new signalR.HubConnectionBuilder().withUrl("/songHub").build();
+
+    document.getElementById("player").play();
+    document.getElementById("player").pause();
+
+
+    connection.on("Play", function () {
+        document.getElementById("msg").innerText = '正在播放！';
+        document.getElementById("player").currentTime = 1;
+        document.getElementById("player").play();
+        document.getElementById("player").currentTime = 1;
+
+    });
+
+
+    connection.on("Pause", function () {
+        document.getElementById("msg").innerText = '已暂停播放！';
+        document.getElementById("player").pause();
+    });
+
+    connection.start().then(function () {
+        document.getElementById("msg").innerText = '连接成功！等待播放！';
+
+
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
