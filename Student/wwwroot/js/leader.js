e166d35c5593f4ad4917cf7fc6843f4523b79f4c
苏震巍 <zsu@senparc.com>
2019-09-02 12:07:43 +0800

    学生端代码

---
+++ Student/wwwroot/js/leader.js
@@ -0,0 +1,24 @@
+﻿
+document.getElementById("play").addEventListener("click", function (event) {
+    connection.invoke("RequestPlay", 0).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("pause").addEventListener("click", function (event) {
+    connection.invoke("RequestPause").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
+document.getElementById("jump").addEventListener("click", function (event) {
+    var currentTime = parseInt(document.getElementById("player").currentTime);
+
+    connection.invoke("RequestPlay", currentTime).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
---
+++ Student/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var connection;
+var ip = "local.senparc.com";
+var connectMsg = '连接成功！等待播放！';
+
+document.getElementById("startConnection").addEventListener("click", function (event) {
+    connection = new signalR.HubConnectionBuilder()
+        .configureLogging(signalR.LogLevel.Information)
+        .withUrl("https://" + ip + "/songHub", { skipNegotiation: true, transport: signalR.HttpTransportType.WebSockets })
+        .build();
+
+    connection.on("Play", function (seconds) {
+        document.getElementById("msg").innerText = '正在播放！';
+        document.getElementById("player").currentTime = seconds;
+        document.getElementById("player").play();
+    });
+
+    connection.on("Pause", function () {
+        document.getElementById("msg").innerText = '已暂停播放！';
+        document.getElementById("player").pause();
+    });
+
+    connection.start().then(function () {
+        document.getElementById("msg").innerText = connectMsg;
+        document.getElementById("playerArea").style.display = 'block';
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
